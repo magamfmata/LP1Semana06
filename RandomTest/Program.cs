b4eacd9a5abf19156802252789630968abee8750
Magali <magamayrmata@gmail.com>
2025-03-27 17:10:54 +0000

    test number 2

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,8 +6,8 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(27);
-            Random r2 = new Random(23);
+            Random r1 = new Random(3);
+            Random r2 = new Random(54);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
