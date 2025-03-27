9fe3255191c56e1f3968e625431b4dda3e97faef
Magali <magamayrmata@gmail.com>
2025-03-27 16:58:27 +0000

    cronometro em segundos

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
 
 namespace Chronos
 {
@@ -6,7 +9,20 @@ namespace Chronos
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+         Stopwatch crono1 = new Stopwatch();
+         Stopwatch crono2 = new Stopwatch();
+
+
+            crono1 .Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+
+            crono1.Stop();
+            crono2.Stop();
+
+            Console.WriteLine("Crono1: {0: F4}", crono1.Elapsed.TotalSeconds);
+            Console.WriteLine("Crono2: {0: F4}", crono2.Elapsed.TotalSeconds);
         }
     }
 }
