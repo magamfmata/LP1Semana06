dbb9a7588d8c1ba857a7c0823e7ff3b1a0d1c8dc
Magali <magamayrmata@gmail.com>
2025-03-27 21:42:20 +0000

    metodos ex9

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace MyGame
 {
     public class Enemy
@@ -7,8 +9,9 @@ namespace MyGame
     private float shield;
     // Construtor
     public Enemy(string name)
-
+    
         {
+        SetName(name);
         this.name = name; // Porque razao usamos "this"?
         health = 100;
         shield = 0;
@@ -20,21 +23,29 @@ namespace MyGame
             return name;
         }
 
-        public void TakeDamage(float damage)
+        public float GetHealth()
         {
-                shield -= damage; 
-                if (shield < 0)
-                {
-                    float damageStillToInflict = -shield; 
-                    shield = 0; 
-                    health -= damageStillToInflict; 
-                    if (health < 0) health = 0; 
-                  
-                }
-           
-            }
+            return health;
+
         }
 
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public string SetName(string newName)
+        {
+            string oldName = "";
+            for (int i = 0; i < newName.Length && i < 8; i++)
+            {
+                oldName += newName[i];
+            }
+            name = oldName;
+            return oldName;
+
+        }
+ }
     }
 
 
