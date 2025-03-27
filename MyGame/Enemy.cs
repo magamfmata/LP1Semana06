eb283822473291f0fd0b133b187d217cdbc50d4c
Magali <magamayrmata@gmail.com>
2025-03-27 21:01:56 +0000

    ex 7

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,5 +1,20 @@
 namespace MyGame
 {
     public class Enemy
-    {}
+    {
+    private string name;
+    private float health;
+    private float shield;
+    // Construtor
+    public Enemy(string name)
+
+        {
+        this.name = name; // Porque razao usamos "this"?
+        health = 100;
+        shield = 0;
+
+        }
+
+    }
+
 }
