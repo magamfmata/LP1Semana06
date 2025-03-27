20e820fbaf60d8786c402dafc5a1d1e1f24c1457
Magali <magamayrmata@gmail.com>
2025-03-27 22:23:03 +0000

    PowerUp added

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,46 +1,40 @@
-using System.Security.Cryptography.X509Certificates;
+using System;
 
 namespace MyGame
 {
     public class Enemy
     {
-    private string name;
-    private float health;
-    private float shield;
-    // Construtor
-    public Enemy(string name)
-    
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy (string name)
         {
-        SetName(name);
-        this.name = name; // Porque razao usamos "this"?
-        health = 100;
-        shield = 0;
-
+            SetName(name);
+            health = 100;
+            shield = 0;
         }
-        public string TakeDamage(float damage)
 
-        {
-            shield -= damage;
-            if (shield <= 0)
-            {
-                float damageLeft = -shield;
-                shield = 0;
-                health -= damageLeft;
-                if (health < 0) health = 0;
-            }
-
-            return health + " " + shield;
-
-        }
         public string GetName()
         {
             return name;
         }
 
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield <= 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
         public float GetHealth()
         {
             return health;
-
         }
 
         public float GetShield()
@@ -48,18 +42,47 @@ namespace MyGame
             return shield;
         }
 
-        public string SetName(string newName)
+        public void SetName(string newName)
         {
-            string oldName = "";
+           string ShortenedName = "";
             for (int i = 0; i < newName.Length && i < 8; i++)
             {
-                oldName += newName[i];
+                ShortenedName += newName[i];
             }
-            name = oldName;
-            return oldName;
-
+            name = ShortenedName;
         }
- }
+
+        public void PickupPowerUp(PowerUp powerUp, float value)
+        {
+            if (powerUp == PowerUp.Health)
+            {
+                health += value;
+                if (health > 100)
+                {
+                    health = 100;
+                }
+            }
+            else if (powerUp == PowerUp.Shield)
+            {
+                shield += value;
+                if (shield > 100)
+                {
+                    shield = 100;
+                }
+            }
+
+            powerUpCount++;
+        }
+
+        private static int powerUpCount;
+        static Enemy()
+        {
+            powerUpCount = 0;
+        }
+
+        public static int GetPowerUpCount()
+        {
+            return powerUpCount;
+        }
     }
-
-
+}
