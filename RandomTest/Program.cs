431d770fe45e994ac104794e5cfbc617926fbbd7
Magali <magamayrmata@gmail.com>
2025-03-27 17:10:11 +0000

    Tried to change

---
+++ RandomTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomTest
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r1 = new Random(27);
+            Random r2 = new Random(23);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r1.Next());
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r2.Next());
+        }
+    }
+}
