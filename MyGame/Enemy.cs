ec1b60739354776592290f928996feb890b28d0c
Magali <magamayrmata@gmail.com>
2025-03-27 21:15:46 +0000

    metodos

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -15,6 +15,26 @@ namespace MyGame
 
         }
 
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {
+                shield -= damage; 
+                if (shield < 0)
+                {
+                    float damageStillToInflict = -shield; 
+                    shield = 0; 
+                    health -= damageStillToInflict; 
+                    if (health < 0) health = 0; 
+                  
+                }
+           
+            }
+        }
+
     }
 
-}
+
