bf0e4b06709c3296f0b785bc4155b08a3520551f
Magali <magamayrmata@gmail.com>
2025-03-28 17:43:54 +0000

    Class Level

--- MyRoguelike/Enemy.cs
+++ MyRoguelike/Enemy.cs
@@ -2,87 +2,17 @@ using System;
 
 namespace MyRoguelike
 {
-public class Enemy
-{
-    private string name;
-    private float health;
-    private float shield;
-
-    public Enemy (string name)
+    public class Enemy
     {
-        SetName(name);
-        health = 100;
-        shield = 0;
-    }
+        private string name;
+        private float health;
+        private float shield;
+        private static int totalPowerUps;
 
-    public string GetName()
-    {
-        return name;
-    }
-
-    public void TakeDamage(float damage)
-    {
-        shield -= damage;
-        if (shield <= 0)
+        public Enemy(string name)
         {
-            float damageStillToInflict = -shield;
-            shield = 0;
-            health -= damageStillToInflict;
-            if (health < 0) health = 0;
+            this.name = name;
+            health = 100;
         }
     }
-
-    public float GetHealth()
-    {
-        return health;
-    }
-
-    public float GetShield()
-    {
-        return shield;
-    }
-
-    public void SetName(string newName)
-    {
-        string ShortenedName = "";
-        for (int i = 0; i < newName.Length && i < 8; i++)
-        {
-            ShortenedName += newName[i];
-        }
-        name = ShortenedName;
-    }
-
-    public void PickupPowerUp(PowerUp powerUp, float value)
-    {
-        if (powerUp == PowerUp.Health)
-        {
-            health += value;
-            if (health > 100)
-            {
-                health = 100;
-            }
-        }
-        else if (powerUp == PowerUp.Shield)
-        {
-            shield += value;
-            if (shield > 100)
-           {
-            shield = 100;
-           }
-        }
-
-        powerUpCount++;
-    }
-
-    private static int powerUpCount;
-    static Enemy()
-    {
-        powerUpCount = 0;
-    }
-
-    public static int GetPowerUpCount()
-    {
-        return powerUpCount;
-    }
-}
 }
